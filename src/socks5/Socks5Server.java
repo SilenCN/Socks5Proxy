e64d8eaa19cec9e8c509c0e99234b50deaf69ffb
1039795376@qq.com <li2302838307>
2019-08-03 22:15:38 +0800

    SSL双向验证

--- src/socks5/Socks5Server.java
+++ src/socks5/Socks5Server.java
@@ -1,20 +1,28 @@
 package socks5;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.concurrent.Semaphore;
 
 public class Socks5Server {
-    private static final int DEFAULT_PORT=1080;
+    private static final int DEFAULT_PORT=1081;
     private static final int MAX_CONNECTION=100;
     public static final byte PROTOCOL_SOCKS5=0x05;
     public static final byte SUPPORT_METHODS[]={0x00,0x02};
     private static boolean FLAG=true;
     public static final String[][] SUPPORT_USER={{"test","test"}};
-    public static void startServer() throws InterruptedException, IOException {
+    public static void startServer() throws InterruptedException, IOException, UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         FLAG=true;
-        ServerSocket serverSocket=new ServerSocket(DEFAULT_PORT);
+        ServerSocket serverSocket=getSSLServerSocket();
         Semaphore semaphore=new Semaphore(MAX_CONNECTION);
         while (FLAG){
             semaphore.acquire();
@@ -23,7 +31,22 @@ public class Socks5Server {
             new ClientSocketThread(client,semaphore).start();
         }
     }
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws InterruptedException, IOException, UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         Socks5Server.startServer();
+    }
+    private static ServerSocket getSSLServerSocket() throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException, CertificateException {
+        SSLContext ctx = SSLContext.getInstance("SSL");
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        KeyStore ks = KeyStore.getInstance("JKS");
+        KeyStore tks = KeyStore.getInstance("JKS");
+        ks.load(new FileInputStream("C:\\Users\\lixinyu\\kserver.keystore"), "qianxin.com".toCharArray());
+        tks.load(new FileInputStream("C:\\Users\\lixinyu\\tserver.keystore"), "qianxin.com".toCharArray());
+        kmf.init(ks, "qianxin.com".toCharArray());
+        tmf.init(tks);
+        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+        SSLServerSocket serverSocket = (SSLServerSocket) ctx.getServerSocketFactory().createServerSocket(DEFAULT_PORT);
+        serverSocket.setNeedClientAuth(true);
+        return serverSocket;
     }
 }
